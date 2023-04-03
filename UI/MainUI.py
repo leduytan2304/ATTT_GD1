74d508769b3c1393743c87962418ae949bf149d2
leduytan2304 <74915968+leduytan2304@users.noreply.github.com>
2023-04-03 13:15:50 +0700

    cập nhật MainUI
    thêm cái bảng viewTable để hiện số người dùng hiện tại

--- UI/MainUI.py
+++ UI/MainUI.py
@@ -1,20 +1,11 @@
 import tkinter as tk
 import oracledb
-
-
+import TreeView as tv
 from tkinter import *
 
         
-root = tk.Tk()
-root.title("Oracle Login")
-root.geometry("300x200")
 
-def on_enter(event):
-    Role.config(bg ='#343434', fg = '#ffffff', )  
-    #print("hi")
-def on_leave(event):
-    #print("hi#")
-    Role.config(bg ='#343434', fg = '#ffffff', ) 
+
 
 def login():
     username = username_entry.get()
@@ -28,20 +19,36 @@ def login():
             "password": password,
             "mode": oracledb.SYSDBA }
             connection = oracledb.connect(**dsn)
+            cursor = connection.cursor()
             print("Login success")
-            return True
+            cursor.execute("SELECT * FROM NHANVIEN")
+
+
+            rows = cursor.fetchall()
+
+
+            for row in rows:
+                print(row)
+            root.destroy()
+            cursor.close()
+            connection.close()
+            afterLogin(username, password)
     except oracledb.DatabaseError as e:
             print("Login fail")
             return False
+    
+
 def afterLoginUI(newRoot):
      pass
      
 
-def afterLogin():
-    root.destroy()
+def afterLogin(username, password):
+
+
+    #sau khi dang nhap hien 1 cửa sổ mới
     newRoot = Tk()
     newRoot.title("Hệ thống quản lý bệnh viện")
-    newRoot.geometry("400x400")
+    newRoot.geometry("700x520")
     my_menu = Menu(newRoot)
     newRoot.config(menu = my_menu)
 
@@ -50,7 +57,7 @@ def afterLogin():
 
     role = Menu(my_menu)
 
-    my_menu.add_cascade(label ="Role", menu = role)
+    my_menu.add_cascade(label ="Role", menu = role)# tao menu Role
     role.add_command(label ="Create",command = our_command)
     role.add_command(label ="Drop",command = our_command)
     role.add_command(label ="Grant",command = newRoot.quit)
@@ -58,7 +65,7 @@ def afterLogin():
 
     privileges = Menu(my_menu)
 
-    my_menu.add_cascade(label ="Privilege", menu = privileges)
+    my_menu.add_cascade(label ="Privilege", menu = privileges)# tao menu Privilege
     privileges.add_command(label ="Grant User Object Privilege",command = our_command)
     privileges.add_command(label ="Grant Role Privilege",command = newRoot.quit)
     privileges.add_command(label ="Grant Role Privilege for user",command = newRoot.quit)
@@ -66,16 +73,25 @@ def afterLogin():
 
     Check_privilege = Menu(my_menu)
 
-    my_menu.add_cascade(label ="Check Privilege", menu = Check_privilege)
+    my_menu.add_cascade(label ="Check Privilege", menu = Check_privilege)# tao menu Check Privilege
     Check_privilege.add_command(label ="Check privilege of User", command = our_command)
     Check_privilege.add_command(label ="Check privilege of Role",command = newRoot.quit)
-    newRoot.mainloop()
+    
+    
+    tv.TreeView(newRoot,username, password)
+    
+
+    
+   
 
 
 
 
 
 
+root = tk.Tk()
+root.title("Oracle Login")
+root.geometry("300x200")
 
 
 username_label = tk.Label(root, text="Username:")
@@ -86,7 +102,7 @@ password_label = tk.Label(root, text="Password:")
 password_label.pack()
 password_entry = tk.Entry(root, show="*")
 password_entry.pack()
-login_button = tk.Button(root, text="Login", command=afterLogin)
+login_button = tk.Button(root, text="Login", command=login)
 login_button.pack()
 
     
