ad28cefcf01f4e4012a52ab19b4546c588f5da1d
leduytan2304 <74915968+leduytan2304@users.noreply.github.com>
2023-04-07 21:47:29 +0700

    Squashed commit of the following:
    commit 524458756a454f4c1796faa358fd5c36f8a17326
    Merge: 6ab1e91 4d3dc0e
    Author: leduytan2304 <74915968+leduytan2304@users.noreply.github.com>
    Date:   Fri Apr 7 21:46:35 2023 +0700
    
        Merge branch 'LDTan'
    
    commit 6ab1e91344d21538248fbb5e9294c7776ff9e091
    Author: leduytan2304 <74915968+leduytan2304@users.noreply.github.com>
    Date:   Fri Apr 7 21:36:07 2023 +0700
    
        Squashed commit of the following:
    
        commit c17e10cb90eea59fe5c38802d55f6eb0275ef47e
        Author: Dat <datk1912@gmail.com>
        Date:   Fri Apr 7 20:58:47 2023 +0700
    
            update Revoke
    
    commit a8dc79dce6ed9c2e666655d6b8714cf44b30ca8d
    Merge: a2430ed bca97d0
    Author: leduytan2304 <74915968+leduytan2304@users.noreply.github.com>
    Date:   Fri Apr 7 21:35:14 2023 +0700
    
        Merge branch 'PTMinh'
    
    commit bca97d002110448954dbb20796fa3b5d74f78f24
    Author: Minh <minh2209ak@gmail.com>
    Date:   Fri Apr 7 19:11:02 2023 +0700
    
        Fix Drop User
    
    commit 03fe70f012c84bbd497eacf35ab3c3fee6089daf
    Author: Minh <minh2209ak@gmail.com>
    Date:   Fri Apr 7 18:04:11 2023 +0700
    
        Final Create, Drop Role. Bugs: Drop User
    
    commit f539a6cc0a27d758f0eec99c81af59ce862d3b11
    Author: Minh <minh2209ak@gmail.com>
    Date:   Thu Apr 6 10:12:53 2023 +0700
    
        Update Create User, User List, Table List
    
    commit a2430edfc5773773136873acb93368b1bd5cce43
    Author: leduytan2304 <74915968+leduytan2304@users.noreply.github.com>
    Date:   Tue Apr 4 22:44:43 2023 +0700
    
        xóa cái cuối bên privilege menu
    
        xóa cái cuối bên privilege menu
    
    commit c4f12f8cafd167c5249d1b9debc32aef1981561a
    Author: Minh <minh2209ak@gmail.com>
    Date:   Tue Apr 4 11:12:38 2023 +0700
    
        add file minh.py
    
    commit 6119be5de5ff000f7bc5efc090037f2661ad225b
    Author: Minh <minh2209ak@gmail.com>
    Date:   Tue Apr 4 11:11:18 2023 +0700
    
        Create User

--- UI/TreeView.py
+++ UI/TreeView.py
@@ -1,31 +1,82 @@
-
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 import oracledb
+
 def TreeView(root, userName, passWord):   
     try:
         dsn = {
-                "host": "localhost",
-                "port": "1521",
-                "sid": "xe",
-                "user": userName,
-                "password": passWord,
-                "mode": oracledb.SYSDBA }
+            "host": "localhost",
+            "port": "1521",
+            "sid": "xe",
+            "user": userName,
+            "password": passWord,
+            "mode": oracledb.SYSDBA }
         connection = oracledb.connect(**dsn)
         cursor = connection.cursor()
-        print("Login success")
-        cursor.execute("SELECT * FROM NHANVIEN")
+        cursor.execute("select username, user_id, account_status, created from DBA_users")
         global rows
         rows = cursor.fetchall()
         total = cursor.rowcount
-        print("total data entries: "+ str(total))
-        for row in rows:
-                print(row)
         
         scrollBarX = Scrollbar(root, orient=HORIZONTAL)
         scrollBarY = Scrollbar(root, orient=VERTICAL)
 
+        my_tree = ttk.Treeview(root)
+        my_tree.place(relx = 0.01,rely = 0.128, width = 646, height = 410)
+        my_tree.configure(yscrollcommand= scrollBarY.set, xscrollcommand= scrollBarX.set)
+        my_tree.configure(selectmode="extended")
+
+        scrollBarY.configure(command=my_tree.yview)
+        scrollBarX.configure(command=my_tree.xview)
+        # my_tree.configure(selectmode="extended")
+
+        scrollBarY.place(relx = 0.934 , rely = 0.128, width = 22, height = 432)
+        scrollBarX.place(relx = 0.002 , rely = 0.922, width = 651, height = 22)
+
+        my_tree.configure(
+            columns=
+            ( "USERNAME", 
+            "USER ID",
+            "ACCOUNT STATUS", 
+            "CREATED DATE" )
+        )
+
+        my_tree.heading("#0", text = "USERNAME",anchor=W)
+        my_tree.heading("USER ID", text = "USER ID",anchor=W)
+        my_tree.heading("ACCOUNT STATUS", text = "ACCOUNT STATUS",anchor=W)
+        my_tree.heading("CREATED DATE", text = "CREATED DATE",anchor=W)
+        for i in rows:
+            my_tree.insert('','end', values = i)
+
+
+        
+        #edit column 
+        my_tree.column("#0",stretch=NO, minwidth=0,width=0)
+        my_tree.column("#1",stretch=NO, minwidth=0,width=125)
+        my_tree.column("#2",stretch=NO, minwidth=25,width=125)
+        my_tree.column("#3",stretch=NO, minwidth=25,width=125)
+        
+        root.mainloop()
+
+    except:
+        print("Error")
+
+    
+
+def tableList(root, userName, passWord):   
+    try:
+        cur = active_login(userName, passWord)
+        cur.execute("SELECT USER, table_name FROM user_tables")
+        global rows
+        rows = cur.fetchall()
+        total = cur.rowcount
+        print("total data entries: "+ str(total))
+        for row in rows:
+            print(row)
+        
+        scrollBarX = Scrollbar(root, orient=HORIZONTAL)
+        scrollBarY = Scrollbar(root, orient=VERTICAL)
 
 
         my_tree = ttk.Treeview(root)
@@ -36,62 +87,58 @@ def TreeView(root, userName, passWord):
 
         scrollBarY.configure(command=my_tree.yview)
         scrollBarX.configure(command=my_tree.xview)
-        # my_tree.configure(selectmode="extended")
-
+        my_tree.configure(selectmode="extended")
 
         scrollBarY.place(relx = 0.934 , rely = 0.128, width = 22, height = 432)
         scrollBarX.place(relx = 0.002 , rely = 0.922, width = 651, height = 22)
 
-
         my_tree.configure(
-            columns=
-            (
-            "MANV", 
-            "TENNV",
-            "PHAI", 
-            "NGAYSINH", 
-            "DIACHI"
-            )
+            columns=("OWNER", 
+            "TABLE NAME")
         )
 
+        my_tree.heading("#0", text = "OWNER", anchor=W)
+        my_tree.heading("#1", text = "TABLE NAME", anchor=W)
 
-        my_tree.heading("#0", text = "ID",anchor=W)
-        my_tree.heading("MANV", text = "MANV",anchor=W)
-        my_tree.heading("TENNV", text = "TENNV",anchor=W)
-        my_tree.heading("PHAI", text = "PHAI",anchor=W)
-        my_tree.heading("NGAYSINH", text = "NGAYSINH",anchor=W)
-        my_tree.heading("DIACHI", text = "DIACHI",anchor=W)
         for i in rows:
             my_tree.insert('','end', values = i)
 
+        def on_select2(event):
+            item = my_tree.focus()
+            text = my_tree.item(item, "values")
+            first_paramater = text[0]
+            print(first_paramater)
+            return first_paramater
 
-
+        my_tree.bind("<<TreeviewSelect>>", on_select2)
+        res = on_select2(None)
+        print(res)
+        
         #edit column 
         my_tree.column("#0",stretch=NO, minwidth=0,width=0)
         my_tree.column("#1",stretch=NO, minwidth=0,width=125)
-        my_tree.column("#2",stretch=NO, minwidth=25,width=125)
-        my_tree.column("#3",stretch=NO, minwidth=25,width=125)
-        my_tree.column("#4",stretch=NO, minwidth=25,width=125)
-
-
         root.mainloop()
 
-        # connStr = cx_Oracle.connect("sys/tan123@localhost:1521/xepdb1", mode=cx_Oracle.SYSDBA)
-        # print("you are connected")
-        # #create a cursor
-        # cursor = connStr.cursor()
-        # sqlTxt = 'select * from NHANVIEN'
-        # cursor.execute(sqlTxt)
-        # rows = cursor.fetchall()
-        # total = cursor.rowcount
-        # print("total data entries: "+ str(total))
+        
+    except Exception as e:
+        print("An error occurred:", e)
+        res = None
+    return res
 
-        # connStr.commit()
 
-        #print the results
-    except:
-        pass
-#root = tk.Tk()
-#TreeView(root, 'sys', 'tan123')
-#connection.close()
+def active_login(username, password):
+    try:
+        dsn = {
+            "host": "localhost",
+            "port": "1521",
+            "sid": "xe",
+            "user": username,
+            "password": password,
+            "mode": oracledb.SYSDBA }
+        connection = oracledb.connect(**dsn)
+        cursor = connection.cursor()
+        return cursor
+        
+    except Exception as e:
+        return None
 
