cae2458cea7adcfa2aa5387473c357135e264fe1
leduytan2304 <74915968+leduytan2304@users.noreply.github.com>
2023-05-07 14:16:20 +0700

    Update LDT_NhanSu.py

--- UI/LDT/LDT_NhanSu.py
+++ UI/LDT/LDT_NhanSu.py
@@ -189,7 +189,7 @@ def UI(username, password):
     TenPB.place(x=60, y = 80)
     TenPB.config(font=("Arial", 12))
     TenPB_entry = tk.Entry(root)
-    TenPB_entry.place(x=160, y = 90)
+    TenPB_entry.place(x=160, y = 85)
 
     TruongPB = tk.Label(root, text="TruongPB")
     TruongPB.place(x=60, y = 120)
@@ -233,49 +233,114 @@ def UI(username, password):
 def UI_2(username, password):  
 
     root = tk.Tk()
-    root.title("Grant privileges on column for user")
-    root.geometry("400x320")       
-    MaPB = tk.Label(root, text="MaPB")
-    MaPB.place(x=60, y = 40)
-    MaPB.config(font=("Arial", 12))
-    MaPB_entry = tk.Entry(root)
-    MaPB_entry.place(x=160, y = 45)
+    root.title("UPDATE NHANVIEN INFORMATION")
+    root.geometry("420x520") 
+    MANV = tk.Label(root, text="MANV")
+    MANV.place(x=60, y = 40)
+    MANV.config(font=("Arial", 12))
+    MANV_entry = tk.Entry(root)
+    MANV_entry.place(x=160, y = 45)
+
+    TENNV = tk.Label(root, text="TENNV")
+    TENNV.place(x=60, y = 80)
+    TENNV.config(font=("Arial", 12))
+    TENNV_entry = tk.Entry(root)
+    TENNV_entry.place(x=160, y = 85)
 
 
-    TenPB = tk.Label(root, text="TenPB")
-    TenPB.place(x=60, y = 80)
-    TenPB.config(font=("Arial", 12))
-    TenPB_entry = tk.Entry(root)
-    TenPB_entry.place(x=160, y = 90)
+    PHAI = tk.Label(root, text="PHAI")
+    PHAI.place(x=60, y = 120)
+    PHAI.config(font=("Arial", 12))
+    PHAI_entry = tk.Entry(root)
+    PHAI_entry.place(x=160, y = 125)
 
-    TruongPB = tk.Label(root, text="TruongPB")
-    TruongPB.place(x=60, y = 120)
-    TruongPB.config(font=("Arial", 12))
-    TruongPB_entry = tk.Entry(root)
-    TruongPB_entry.place(x=160, y = 125)
+    NGAYSINH = tk.Label(root, text="NGAYSINH(YYYY-MM-DD)")
+    NGAYSINH_DES = tk.Label(root, text="(YYYY-MM-DD)")
+    NGAYSINH_DES.place(x=90, y = 160)
+    NGAYSINH.place(x=60, y = 160)
+    NGAYSINH.config(font=("Arial", 12))
+    NGAYSINH_entry = tk.Entry(root)
+    NGAYSINH_entry.place(x=160, y = 165)
+
+    DIACHI = tk.Label(root, text="DIACHI")
+    DIACHI.place(x=60, y = 200)
+    DIACHI.config(font=("Arial", 12))
+    DIACHI_entry = tk.Entry(root)
+    DIACHI_entry.place(x=160, y = 205)
+
+    SoDT = tk.Label(root, text="SoDT")
+    SoDT.place(x=60, y = 240)
+    SoDT.config(font=("Arial", 12))
+    SoDT_entry = tk.Entry(root)
+    SoDT_entry.place(x=160, y = 245)
+
+    VAITRO = tk.Label(root, text="VaiTro")
+    VAITRO.place(x=60, y = 280)
+    VAITRO.config(font=("Arial", 12))
+    VAITRO_entry = tk.Entry(root)
+    VAITRO_entry.place(x=160, y = 285)
+
+    MANQL = tk.Label(root, text="MANQL")
+    MANQL.place(x=60, y = 320)
+    MANQL.config(font=("Arial", 12))
+    MANQL_entry = tk.Entry(root)
+    MANQL_entry.place(x=160, y = 325)
+
+    PHG = tk.Label(root, text="PHG")
+    PHG.place(x=60, y = 360)
+    PHG.config(font=("Arial", 12))
+    PHG_entry = tk.Entry(root)
+    PHG_entry.place(x=160, y = 365)
    
 
     
-
-    Ma_data =MaPB_entry.get()
-
-    TenPB_data = TenPB_entry.get()
-    TruongPB_data = TruongPB_entry.get()
-    def print_data():
-        if(MaPB_entry.get() ==""  or TenPB_entry.get() ==""):
-            print("please input in all the textbox")
-        else:
-            Ma_data =MaPB_entry.get()
-            TenPB_data = TenPB_entry.get()
-            TruongPB_data = TruongPB_entry.get()
-            print(MaPB_entry.get())
-            print(TenPB_entry.get())
-            print(TruongPB_entry.get())
+    MANV_data =MANV_entry.get()    
+    TENNV_data =TENNV_entry.get()
+    PHAI_data = PHAI_entry.get()
+    NGAYSINH_data = NGAYSINH_entry.get()
+    DIACHI_data = DIACHI_entry.get()
+    SoDT_data = SoDT_entry.get()
+    VAITRO_data = VAITRO_entry.get()
+    MANQL_data = MANQL_entry.get()
+    PHG_data = PHG_entry.get()
+    def print_data_2():
+        # if(TENNV_entry.get() == ""  or PHAI_entry.get() =="" or NGAYSINH_entry.get() =="" or DIACHI_entry.get() or  SoDT_entry.get() =="" or VAITRO_entry.get() =="" or PHG_entry.get() =="" or  MANQL_entry.get()=="" or MANV_entry.get()=="" ):
+        #     print("please input in all the textboxx")
+        # else:
+            MANQL_data = MANQL_entry.get()
+            MANV_data = MANV_entry.get()
+            TENNV_data =TENNV_entry.get()
+            PHAI_data = PHAI_entry.get()
+            NGAYSINH_data = NGAYSINH_entry.get()
+            DIACHI_data = DIACHI_entry.get()
+            SoDT_data = SoDT_entry.get()
+            VAITRO_data = VAITRO_entry.get()
+            PHG_data = PHG_entry.get()
+            print(TENNV_entry.get())
+            print(PHAI_entry.get())
+            print(NGAYSINH_entry.get())
+            print(DIACHI_entry.get())
+            print(SoDT_entry.get())
+            print(VAITRO_entry.get())
+            print(PHG_entry.get())
             try:
                 cur = active_login(username, password)
-                sqlTxt = "UPDATE system.PHONGBAN SET TenPB = '" + TenPB_data+ "', TRPHG ='" + TruongPB_data + "' WHERE MAPB = '"+ Ma_data + "'" 
+                # UPDATE NHANVIEN
+                # SET TENNV = 'New Name',
+                #     PHAI = 'New Gender',
+                #     NGAYSINH = TO_DATE('1990-01-01', 'YYYY-MM-DD'),
+                #     DIACHI = 'New ',
+                #     SODT = 1234590,
+                #     LUONG = 'New Salary',
+                #     PHUCAP = 'New ',
+                #     VAITRO = 'New Role',
+                #     MANQL = 'NV0001',
+                #     PHG = 'PB001'
+                # WHERE MANV = 'NV0002';
+                sqlTxt = "UPDATE system.NHANVIEN SET TENNV = '" + TENNV_data+ "', PHAI ='" + PHAI_data +"', NGAYSINH = TO_DATE('" + NGAYSINH_data +"', 'YYYY-MM-DD'), DIACHI ='" + DIACHI_data + "', SODT ='" + SoDT_data + "', VAITRO ='" + VAITRO_data  + "', MANQL ='" + MANQL_data +  "', PHG ='" + PHG_data + "' WHERE MANV = '"+ MANV_data + "'" 
                 print("sql text:" + sqlTxt)
                 cur.execute(sqlTxt)
+                print("sql text:" + sqlTxt)
                 cur.execute("COMMIT")
             except:
                 print("da xay ra loi") 
@@ -283,8 +348,8 @@ def UI_2(username, password):
            
 
 
-    Execute_btn = tk.Button(root, text="Update", command=print_data)# button Drop User
-    Execute_btn.place(x= 180, y = 200)
+    Execute_btn = tk.Button(root, text="Update", command=print_data_2)# button Drop User
+    Execute_btn.place(x= 280, y = 200)
     #SELECT GRANTEE,GRANTED_ROLE FROM DBA_ROLE_PRIVS WHERE GRANTED_ROLE = 'DATAENTRY'
     root.mainloop()
    
@@ -349,15 +414,18 @@ def afterLogin(username, password):
             tableList_LDT(newRoot,username, password)
         def Update_PhongBan():
             UI(username, password)
-
+        def Update_NHANVIEN():
+            UI_2(username, password)
 
         btn3 = tk.Button(newRoot, text="User List", command=userList)
-        btn3.place(x= 400, y = 40)
+        btn3.place(x= 100, y = 40)
 
         btn4 = tk.Button(newRoot, text="Table List", command=tableList)
-        btn4.place(x= 500, y = 40)
+        btn4.place(x= 200, y = 40)
         btn5 = tk.Button(newRoot, text="Update PHONGBAN Info ", command=Update_PhongBan)
-        btn5.place(x= 600, y = 40)
+        btn5.place(x= 300, y = 40)
+        btn6 = tk.Button(newRoot, text="Update NHANVIEN Info ", command=Update_NHANVIEN)
+        btn6.place(x= 500, y = 40)
         TreeView_LDT(newRoot,username, password) # module của TreeView
 
 
