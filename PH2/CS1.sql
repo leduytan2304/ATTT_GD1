c2962224d1f2826d061c52cc0875f2922992fe15
Dat <datk1912@gmail.com>
2023-05-07 19:27:23 +0700

    CSDL

--- PH2/CS1.sql
+++ PH2/CS1.sql
@@ -9,8 +9,8 @@ create or replace view  view_NV_PhanCong as
     select *
     from PhanCong;
 
-Grant SELECT ON view_NhanVien TO NhanVien;
-Grant SELECT ON view_PhanCong TO NhanVien;
+Grant SELECT ON view_NV_NhanVien TO NhanVien;
+Grant SELECT ON view_NV_PhanCong TO NhanVien;
 Grant UPDATE(NGAYSINH, DIACHI, SODT) ON view_NhanVien TO NhanVien;
 Grant SELECT ON PHONGBAN TO NhanVien;
 Grant SELECT ON DEAN TO NhanVien;
