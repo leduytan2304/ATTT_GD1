d593c307dd85b79bf30ef12aee74e1bbdd0166a2
tdkunz <74592421+tdkunz@users.noreply.github.com>
2023-05-07 20:34:12 +0700

    Update cs3.sql

--- UI/PH2/cs3.sql
+++ UI/PH2/cs3.sql
@@ -1,2 +1,11 @@
---grant quyền xem thông tin cá nhân trên view chứa thông tin đã giải mã
-grant select on view_decrypt_NHANVIEN_LUONG to truongphong;
+--tao view xem nhan vien thuoc phong ban cua truong phong
+drop view View_TruongPhong_Select_NhanVien_Info;
+CREATE OR REPLACE VIEW View_TruongPhong_Select_NhanVien_Info AS
+SELECT NV.MANV, NV.TENNV, NV.PHAI, NV.NGAYSINH, NV.DIACHI, NV.SODT, NV.VAITRO, NV.MANQL, NV.PHG
+FROM NHANVIEN NV, PHONGBAN PB, USERNAME USN
+WHERE USN.user_name = SYS_CONTEXT ('USERENV', 'SESSION_USER')
+AND PB.TRPHG = USN.MANV
+AND PB.MAPB = NV.PHG;
+
+--grant quyen xem thong tin tren view xem thong tin nhan vien thuoc phong ban cua truong phong
+GRANT SELECT ON View_TruongPhong_Select_NhanVien_Info to truongphong;
