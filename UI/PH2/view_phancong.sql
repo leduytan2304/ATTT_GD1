bc114825c3ded40eec94f0c2eb3526133b58cac9
Đạt Nguyễn <iloverosebp1102@gmail.com>
2023-05-07 20:48:59 +0700

    file sql

---
+++ UI/PH2/view_phancong.sql
@@ -0,0 +1,38 @@
+--xem thong tin phan cong cua minh
+drop view View_Select_PhanCong_Info;
+CREATE OR REPLACE VIEW View_Select_PhanCong_Info AS
+SELECT PC.*
+FROM PHANCONG PC
+
+--grant quyen xem view phan cong cho cac role
+grant select on View_Select_PhanCong_Info to nhanvien;
+grant select on View_Select_PhanCong_Info to qltructiep;
+grant select on View_Select_PhanCong_Info to truongphong;
+grant select on View_Select_PhanCong_Info to taichinh;
+grant select on View_Select_PhanCong_Info to nhansu;
+grant select on View_Select_PhanCong_Info to truongdean;
+
+--tao vpd cho view phan cong
+BEGIN
+  DBMS_RLS.ADD_POLICY (
+    object_schema   => 'system',
+    object_name     => 'View_Select_PhanCong_Info',
+    policy_name     => 'PHANCONG_Select_Info',
+    function_schema => 'system',
+    policy_function => 'Select_NhanVien',
+    statement_types => 'SELECT',
+    update_check    => TRUE
+  );
+END;
+
+--drop policy
+begin
+    DBMS_RLS.DROP_POLICY (
+        object_schema   => 'system',
+        object_name     => 'PHANCONG',
+        policy_name     => 'PHANCONG_Select_Info');
+<<<<<<< HEAD
+end;
+=======
+end;
+>>>>>>> f142fc1cc3f69272582a645d061eeb53397560ec
